c2f6f628169c10a003a00b5393ac4846162121cd
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2023-08-17 15:26:39 +0200

    update solution for java 17

--- koans/src/commonMain/kotlin/com/rsicarelli/koansbr/classes/sealedClasses/references/SealedClassesJava.java
+++ koans/src/commonMain/kotlin/com/rsicarelli/koansbr/classes/sealedClasses/references/SealedClassesJava.java
@@ -6,14 +6,25 @@
 
 package com.rsicarelli.koansbr.classes.sealedClasses.references;
 
+/**
+ * Classe principal para demonstrar o uso de classes seladas.
+ */
 public class SealedClassesJava {
+
+    /**
+     * Avalia a expressão fornecida.
+     *
+     * @param expr A expressão a ser avaliada.
+     * @return O valor da expressão.
+     * @throws IllegalArgumentException Se o tipo de expressão for desconhecido.
+     */
     static int eval(Expr expr) {
         if (expr instanceof Num) {
             return ((Num) expr).value();
         } else if (expr instanceof Sum) {
             return eval(((Sum) expr).left()) + eval(((Sum) expr).right());
         } else {
-            throw new IllegalArgumentException("Unknown Expr type");
+            throw new IllegalArgumentException("Tipo de expressão desconhecido");
         }
     }
 }
